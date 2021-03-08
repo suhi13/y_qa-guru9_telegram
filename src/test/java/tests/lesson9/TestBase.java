4230466f43800815f867e67986c18725c4765506
Yuliia Sukhova <yuliia.sukhova@tech-stack.io>
2021-03-08 23:39:10 +0200

    lesson9-added Telegram bot

--- src/test/java/tests/lesson9/TestBase.java
+++ src/test/java/tests/lesson9/TestBase.java
@@ -1,28 +1,22 @@
 package tests.lesson9;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-import static helpers.AttachmentHelpers.attachAsText;
-import static helpers.AttachmentHelpers.attachPageSource;
-import static helpers.AttachmentHelpers.attachScreenshot;
-import static helpers.AttachmentHelpers.attachVideo;
-import static helpers.AttachmentHelpers.getConsoleLogs;
-
 import com.codeborne.selenide.Configuration;
-
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import io.qameta.allure.selenide.AllureSelenide;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentHelpers.*;
 
 public class TestBase {
-
     @BeforeAll
     static void setup() {
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.startMaximized = true;
+        System.setProperty("remote_driver", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         if(System.getProperty("remote_driver") != null) {
             // config for Java + Selenide
